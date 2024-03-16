e2517bbec902918d4e78226affddf131379c82b4
Samuelomondi <samuelomondi3@gmail.com>
2024-03-16 18:03:38 +0300

    Removed URL image source n Added to assests: Load issues

--- src/components/main/card-data.js
+++ src/components/main/card-data.js
@@ -1,34 +1,40 @@
-const CardData = [
-    {
-        name:'Givenchy',
-        description:'blue flowly dress',
-        price:'$150.00',
-        picture:'https://images.unsplash.com/photo-1539008835657-9e8e9680c956?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-        name:'Prada',
-        description:'orange bridal dress',
-        price:'$100.00',
-        picture:'https://images.unsplash.com/photo-1596783074918-c84cb06531ca?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-        name:'Shein',
-        description:'green spaghetti dress',
-        price:'$80.00',
-        picture:'https://images.unsplash.com/photo-1596783047904-4000addd05cd?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-        name:'Fashion Nova',
-        description:'white tube dress',
-        price:'$120.00',
-        picture:'https://images.unsplash.com/photo-1631233859262-0d7b12ea7d4c?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-        name:'Free People',
-        description:'red & white floral dress',
-        price:'$170.00',
-        picture:'https://images.unsplash.com/photo-1622122201714-77da0ca8e5d2?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-]
+import Blue from '../../assets/dresses/blue.jpg'
+import Orange from '../../assets/dresses/orange.jpg'
+import Green from '../../assets/dresses/green.jpg'
+import White from '../../assets/dresses/white.jpg'
+import Red from '../../assets/dresses/red.jpg'
 
-export default CardData
+const CardData = [
+  {
+    name: "Givenchy",
+    description: "blue flowly dress",
+    price: "$150.00",
+    picture: Blue,
+  },
+  {
+    name: "Prada",
+    description: "orange bridal dress",
+    price: "$100.00",
+    picture: Orange,
+  },
+  {
+    name: "Shein",
+    description: "green spaghetti dress",
+    price: "$80.00",
+    picture: Green,
+  },
+  {
+    name: "Fashion Nova",
+    description: "white tube dress",
+    price: "$120.00",
+    picture: White,
+  },
+  {
+    name: "Free People",
+    description: "red & white floral dress",
+    price: "$170.00",
+    picture: Red,
+  }
+];
+
+export default CardData;
